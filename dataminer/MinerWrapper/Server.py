524c82d60a04b05a80186d395e4692414d3b3188
Justin Duplessis <drfoliberg@gmail.com>
2014-02-12 15:03:58 -0500

    Added config sanity check
    Check if same number of devices is reported by cgminer and the local
    config.

--- dataminer/MinerWrapper/Server.py
+++ dataminer/MinerWrapper/Server.py
@@ -18,12 +18,20 @@ class Handler(BaseHTTPRequestHandler):
         server_id = "id"
         server_status = {"uptime": 12124, 'load_avg':[0.12, 0.10, 0.05]}
         devs = api.getDevsArray()
-        gpu_statuses = GpuInfo.processDevs(devs)
-        res = {'timestamp':timestamp, 'server_id': server_id, 'server_status':server_status, 'gpu_statuses':gpu_statuses}
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(json.dumps(res))
+        # check if config has the same number of gpu as cginer reports
+        if len(devs) != len(ServerInfo.getGpus()):
+            print "Config is not sane!"
+            self.send_response(500)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write("Config is not sane!")
+        else:   
+            gpu_statuses = GpuInfo.processDevs(devs)
+            res = {'timestamp':timestamp, 'server_id': server_id, 'server_status':server_status, 'gpu_statuses':gpu_statuses}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(res))
 
 def serve_on_port(port):
     print "Serving on local host port %s" % str(port)
--- dataminer/MinerWrapper/ServerInfo.py
+++ dataminer/MinerWrapper/ServerInfo.py
@@ -14,10 +14,20 @@ class ServerInfo():
     
     def getGPUGlobalId(self, relativeId):
         # json_data=open('config')
-        json_data = json.dumps(
-        [{'relative':'0', 'global':'1234'},
-        {'relative':'1', 'global':'1235'}])
+        json_data = self.getConfig()
         data = json.loads(json_data)
         if len(data) > relativeId:
             return data[relativeId]['global']
         return -1
+
+
+    def getConfig(self):
+        return json.dumps([
+        {'relative':'0', 'global':'1234'},
+        {'relative':'1', 'global':'1235'}])
+    
+    
+    def getGpus(self):
+        json_data = self.getConfig()
+        return json.loads(json_data)
+        
