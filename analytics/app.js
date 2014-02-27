423a707fdb01b3ca63d7b7cb76226aaba32b05ad
petitprince13 <felix.leblanc1305@gmail.com>
2014-02-26 20:42:08 -0500

    Added the sql queries

--- analytics/app.js
+++ analytics/app.js
@@ -14,10 +14,34 @@ connection = mysql.createConnection({
  
 function insert_stats(data){
   //Insert in the stats table
+  var stats_query = 'insert into stats(device_id, temperature,' +
+  'device_voltage, engine_clock, memory_clock, fan_rpm,' +
+  'hardware_errors, shares_rejected, shares_accepted, hashrate,' +
+  'intensity, time_since_last_work, time_since_last_valid_work) ' +
+  'values(' + data.device_id + ',' + data.temperature + ',' +
+  data.device_voltage + ',' + data.engine_clock + ',' +
+  data.memory_clock + ',' + data.fan_rpm + ',' +
+  data.hardware_errors + ',' + data.shares_rejected + ',' +
+  data.shares_accepted + ',' + data.hashrate + ',' +
+  data.intensity + ',' + data.time_since_last_work + ',' +
+  data.time_since_last_valid_work + ');';
+  connection.query(stats_query, function(err, rows, fields){
+    if(err){
+      console.log(err);
+    }
+  });
 }
  
 function insert_server_stats(data){
   //Insert in the server_stats table
+  var stats_machines_query = 'insert into stats_machines(machine_id,' +
+  'uptime, load_avg, timestamp) values(' + machine_id + ',' +
+  uptime + ',' + load_avg + ',' + timestamp + ');';
+  connection.query(stats_machines_query, function(err, rows, fields){
+    if(err){
+      console.log(err);
+    }
+  });
 }
   
 function insert_data(err, res, body){
@@ -63,3 +87,7 @@ async.each(get_machines_ip, ip_iterator, function(err){
       console.log(err);
     }
 });
+
+connection.end();
+
+process.exit();
