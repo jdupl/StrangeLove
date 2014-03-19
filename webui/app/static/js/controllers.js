e3c461236eb2d18b01f49526ac21b6fd9eef592f
drfoliberg <drfoliberg@gmail.com>
2014-03-18 20:27:45 -0400

    devices/servers view and controller implementation

--- webui/app/static/js/controllers.js
+++ webui/app/static/js/controllers.js
@@ -4,19 +4,24 @@
 
 angular.module('strangelove.controllers', []).
         controller('devices', function($scope) {
+
+            $scope.mySelections = [];
+
             $scope.devices = [
-                {'name': 'carte1', 'hashrate': '120'},
-                {'name': 'carte2', 'hashrate': '150'},
-                {'name': 'carte3', 'hashrate': '20'},
-                {'name': 'carte4', 'hashrate': '0'},
-                {'name': 'carte5', 'hashrate': '1'},
-                {'name': 'carte6', 'hashrate': '10'},
-                {'name': 'carte7', 'hashrate': '50'},
-                {'name': 'carte8', 'hashrate': '12'}
+                {'id': '1', 'model': 'R9 270x', 'hashrate': '470', 'validShares': '1500', 'invalidShares': '10', 'owners': [{'name': 'Justin', 'share': '100%'}]},
+                {'id': '2', 'model': 'R9 280x', 'hashrate': '760', 'validShares': '3500', 'invalidShares': '15', 'owners': [{'name': 'Félix', 'share': '25%'}, {'name': 'Maxime', 'share': '25%'}, {'name': 'Simon', 'share': '25%'}, {'name': 'Guillaume', 'share': '25%'}]}
             ];
+
             $scope.gridOptions = {
                 data: 'devices',
-                columnDefs: [{field: 'name', displayName: 'Name'}, {field: 'hashrate', displayName: 'Hashrate'}],
+                columnDefs: [
+                    {field: 'id', displayName: 'Id Gpu'},
+                    {field: 'model', displayName: 'Modèle de GPU'},
+                    {field: 'hashrate', displayName: 'Hashrate'},
+                    {field: 'validShares', displayName: 'Shares acceptés'},
+                    {field: 'invalidShares', displayName: 'Shares refusés'}
+                ],
+                selectedItems: $scope.mySelections,
                 multiSelect: false
             };
             $scope.getTableStyle = function() {
@@ -24,6 +29,33 @@ angular.module('strangelove.controllers', []).
                 var headerHeight = 45;
                 return {
                     height: ($scope.devices.length * rowHeight + headerHeight) + "px"
+                };
+            };
+        })
+        .controller('servers', function($scope) {
+
+            $scope.mySelections = [];
+
+            $scope.servers = [
+                {'id': '1', 'uptime': '15 jours 7 heures', 'admin': 'Justin', 'freeSlots': '0'}
+            ];
+
+            $scope.gridOptions = {
+                data: 'servers',
+                columnDefs: [
+                    {field: 'id', displayName: 'Id serveur'},
+                    {field: 'uptime', displayName: 'Uptime'},
+                    {field: 'admin', displayName: 'Admin Responsable'},
+                    {field: 'freeSlots', displayName: 'Ports libres'}
+                ],
+                selectedItems: $scope.mySelections,
+                multiSelect: false
+            };
+            $scope.getTableStyle = function() {
+                var rowHeight = 30;
+                var headerHeight = 45;
+                return {
+                    height: ($scope.servers.length * rowHeight + headerHeight) + "px"
                 };
             };
         })
